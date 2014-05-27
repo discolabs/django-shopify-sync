bc0ff42a104abe8075a22c2c54dd69b866848b5f
Gavin Ballard <gavin@gavinballard.com>
2014-05-27 21:21:59 +0200

    Tweak packaging and requirements.

--- setup.py
+++ setup.py
@@ -1,33 +1,33 @@
 from distutils.core import setup
 
-version=__import__('shopify_sync').__version__
-
+version = __import__('shopify_sync').__version__
 setup(
-    name='django-shopify-sync',
-    version=version,
-    description='A package for synchronising Django models with Shopify resources.',
-    long_description=open('README.rst').read(),
-    author='Gavin Ballard',
-    author_email='gavin@gavinballard.com',
-    url='https://github.com/gavinballard/django-shopify-sync',
-    license='MIT',
+    name = 'django-shopify-sync',
+    version = version,
+    description = 'A package for synchronising Django models with Shopify resources.',
+    long_description = open('README.rst').read(),
+    author = 'Gavin Ballard',
+    author_email = 'gavin@gavinballard.com',
+    url = 'https://github.com/gavinballard/django-shopify-sync',
+    license = 'MIT',
 
-    packages=['shopify_sync'],
-    package_dir={
+    packages = ['shopify_sync'],
+    package_dir = {
         'shopify_sync': 'shopify_sync',
     },
 
-    requires=['Django (>=1.3)',],
-    install_requires=['Django>=1.3',],
+    requires = [
+        'django',
+        'shopify',
+        'owned_models',
+        'jsonfield',
+    ],
+
+    install_requires = [
+        'django',
+    ],
 
     zip_safe=True,
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-    ],
+    classifiers=[],
 )
+
