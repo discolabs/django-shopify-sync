64bd1f5b9c2fbc20d9e12d5665737079a90fd486
Gavin Ballard <gavin@gavinballard.com>
2014-07-10 14:59:33 +0200

    Add methods to Product object.

--- shopify_sync/__init__.py
+++ shopify_sync/__init__.py
@@ -1,5 +1,3 @@
 VERSION     = (0, 0, 3)
 __version__ = '.'.join(map(str, VERSION))
 __author__  = 'Gavin Ballard'
-
-from models import *
--- shopify_sync/models/product.py
+++ shopify_sync/models/product.py
@@ -1,5 +1,6 @@
 from .base import ShopifyDatedResourceModel
 from .collect import Collect
+from .variant import Variant
 from django.db import models
 import shopify
 
@@ -20,6 +21,27 @@ class Product(ShopifyDatedResourceModel):
     @property
     def collects(self):
         return Collect.objects.filter_for_user(self.user, product_id = self.id)
+
+    @property
+    def variants(self):
+        return Variant.objects.filter_for_user(self.user, product_id = self.id)
+
+    @property
+    def price(self):
+        return (
+            min([variant.price for variant in self.variants]),
+            max([variant.price for variant in self.variants]),
+        )
+
+    @property
+    def weight(self):
+        return (
+            min([variant.grams for variant in self.variants]),
+            max([variant.grams for variant in self.variants]),
+        )
+
+    def __unicode__(self):
+        return self.title
 
     class Meta:
         app_label = 'shopify_sync'
