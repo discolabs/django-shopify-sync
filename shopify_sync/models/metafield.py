9f74018e081347d34defb2d98b9787b8c464d3da
Gavin Ballard <gavin@gavinballard.com>
2014-11-19 10:03:59 +0100

    Add Metafield resource.

---
+++ shopify_sync/models/metafield.py
@@ -0,0 +1,30 @@
+from django.db import models
+import shopify
+
+from .base import ShopifyDatedResourceModel
+
+
+class Metafield(ShopifyDatedResourceModel):
+    shopify_resource_class = shopify.resources.Metafield
+
+    VALUE_TYPE_STRING = 'string'
+    VALUE_TYPE_INTEGER = 'integer'
+    VALUE_TYPE_CHOICES = (
+        (VALUE_TYPE_STRING, 'String'),
+        (VALUE_TYPE_INTEGER, 'Integer'),
+    )
+
+    OWNER_RESOURCE_SHOP = 'shop'
+    OWNER_RESOURCE_PRODUCT = 'product'
+    OWNER_RESOURCE_CHOICES = (
+        (OWNER_RESOURCE_SHOP, 'Shop'),
+        (OWNER_RESOURCE_PRODUCT, 'Product'),
+    )
+
+    description = models.CharField(max_length = 255)
+    key = models.CharField(max_length = 30)
+    namespace = models.CharField(max_length = 20)
+    owner_id = models.IntegerField()
+    owner_resource = models.CharField(max_length = 32, choices = OWNER_RESOURCE_CHOICES, default = OWNER_RESOURCE_SHOP)
+    value = models.TextField()
+    value_type = models.CharField(max_length = 32, choices = VALUE_TYPE_CHOICES, default = VALUE_TYPE_STRING)
